7bc42856a2af713611a54c3b4f99aec5c8044600
Omarit82 <roselliomar82@gmail.com>
2025-11-25 09:05:47 -0300

    Feature/ Agregado de la clase Puesto y View

--- src/Principal.java
+++ src/Principal.java
@@ -1,5 +1,11 @@
+import objetos.Institucion;
+import view.Visualizacion;
+
 public class Principal {
     public static void main(String[] args) throws Exception {
-       
+        Institucion apnea = new Institucion("Apnea");
+        Visualizacion view = new Visualizacion(apnea);
+
+        view.menuPrincipal();
     }
 }
--- src/objetos/Puesto.java
+++ src/objetos/Puesto.java
@@ -1,5 +1,24 @@
 package objetos;
 
 public class Puesto {
-
+     /*Atributo*/
+    private String nombre;
+    private boolean ocupado;
+    /*Constructor*/
+    public Puesto(String nombre) {
+        this.nombre = nombre;
+        this.ocupado = false;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public boolean isOcupado() {
+        return ocupado;
+    }
+    public void setOcupado(boolean ocupado) {
+        this.ocupado = ocupado;
+    }
 }
